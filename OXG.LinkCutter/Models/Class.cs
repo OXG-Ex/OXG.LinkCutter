c36c27d70a888be158fe2728eda5fb02d5192985
Назаренко Семён <semes212@gmail.com>
2020-05-13 15:48:55 +0300

    Добавьте файлы проекта.

---
+++ OXG.LinkCutter/Models/Class.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OXG.LinkCutter.Models
+{
+    public class Link
+    {
+        public int Id { get; set; }
+
+    }
+}
